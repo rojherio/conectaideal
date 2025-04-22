b0aa37731ae042cf7bf8a6b6c231e1d104848e04
Someone3lse <gabrielmouramarinho@gmail.com>
2025-04-21 21:04:17 -0500

    Dashboard Unidade Organizacional Tipo

--- view/bsc/unidade_organizacional_tipo/dashboard.php
+++ view/bsc/unidade_organizacional_tipo/dashboard.php
@@ -2,84 +2,92 @@
 include_once ('template/topo.php');
 include_once ('template/sidebar.php');
 include_once ('template/header.php');
+$id = !(isset($_POST['id'])) ? 0 : $_POST['id'];
 $db = Conexao::getInstance();
+//Consulta para Edição - BEGIN
 $stmt = $db->prepare("SELECT 
   uot.id AS id, 
   uot.nome AS nome, 
-  uot.status AS status, 
-  uo.bsc_unidade_organizacional_tipo_id AS id_tipo
+  uot.status AS status 
   FROM bsc_unidade_organizacional_tipo AS uot 
-  LEFT JOIN bsc_unidade_organizacional AS uo ON uot.id = uo.bsc_unidade_organizacional_tipo_id
-  GROUP BY uot.id 
+  WHERE uot.id = ? ;");
+$stmt->bindValue(1, $id);
+$stmt->execute();
+$rsUOTipo = $stmt->fetch(PDO::FETCH_ASSOC);
+//Consulta para Edição - END
+//Consulta para DataTable - BEGIN
+$stmt = $db->prepare("
+  SELECT 
+  uot.id AS id,  
+  uot.nome AS nome, 
+  uot.status AS status, 
+  COUNT(uo.id) AS qtd_uo_id 
+  FROM bsc_unidade_organizacional_tipo AS uot 
+  LEFT JOIN bsc_unidade_organizacional AS uo ON uot.id = uo.bsc_unidade_organizacional_tipo_id 
+  GROUP BY uot.id, uot.nome, uot.status 
   ORDER BY uot.nome ASC;");
 $stmt->execute();
-$rsUnidadeOrganizaionalTipo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$rsUOTipos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//Consulta para DataTable - END
 ?>
 <!-- main section -->
 <main>
   <div class="container-fluid">
-    <!-- Breadcrumb start -->
+    <!-- div Título página e links de navegação - BEGIN -->
     <div class="row m-1">
       <div class="col-12 ">
-        <h4 class="main-title">Tipo de instituição</h4>
+        <h4 class="main-title">Tipo de Unidade Organizacional</h4>
         <ul class="app-line-breadcrumbs mb-3">
           <li class="">
             <a href="<?= PORTAL_URL; ?>" class="f-s-14 f-w-500">
               <span>
-                <i class="ph-duotone  ph-cardholder f-s-16"></i>  Estrutura Organizacional
+                <i class="ph-duotone  ph-cardholder f-s-16"></i> Cadastros Básicos
               </span>
             </a>
           </li>
           <li class="active">
-            <a href="<?= PORTAL_URL; ?>" class="f-s-14 f-w-500">Tipo de isntituição</a>
+            <a href="<?= PORTAL_URL; ?>" class="f-s-14 f-w-500">Tipo de Unidade Organizacional</a>
           </li>
         </ul>
       </div>
     </div>
-    <!-- Breadcrumb end -->
-
-    <!-- Floating Labels start -->
+    <!-- div Título página e links de navegação - END -->
+    <!-- div de cadastro - BEGIN -->
     <div class="row">
       <div class="col-md-12">
         <div class="card">
           <div class="card-header">
-            <h5>NOVO TIPO DE INSTITUIÇÃO</h5>
+            <!-- Título da div de cadastro - BEGIN -->
+            <h5>CADASTRO DE TIPO DE UNIDADE ORGANIZACIONAL</h5>
             <small>Explicação da página</small>
+            <!-- Título da div de cadastro - END -->
           </div>
           <div class="card-body">
-            <form class="app-form" id="form_unidade_organizacional_tipo" name="form_unidade_organizacional_tipo" method="post" action="">
+            <!-- formulário de cadastro - BEGIN -->
+            <form class="app-form" id="form_uo_tipo" name="form_uo_tipo" method="post" action="">
+              <!-- div row input - BEGIN -->
               <div class="row">
                 <div class="col-12">
                   <div class="form-floating mb-3">
-                    <input type="text" class="form-control" minlength="3" id="nome_tipo" name="nome_tipo" placeholder="Digite o nome da instituição" value="Alabama" required>
-                    <label for="nome_tipo">Nome</label>
+                    <input type="text" class="form-control" minlength="3" maxlength="100" id="nome_uot" name="nome_uot" placeholder="Digite o nome do tipo de unidade organizacional" value="<?= $rsUOTipo['nome']; ?>" required>
+                    <label for="nome_uot">Nome</label>
                   </div>
                 </div>
               </div>
+              <!-- div row input - END -->
+              <!-- div row checkbox - BEGIN -->
               <div class="row">
-                <div class="col-12">
-                  <div class="form-floating mb-3">
-                    <select class="select_modelo form-control select-example form-select select-basic" id="secretaria" name="secretaria" aria-label="Selecione uma secretaria">>
-                      <option value="AL">Alabama</option>
-                      <option value="AL">Wyoming</option>
-                      <option value="WY">Coming</option>
-                      <option value="WY">Hanry Die</option>
-                      <option value="WY">John Doe</option>
-                    </select>
-                    <label for="secretaria" class="form-label">Secretaria</label>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-12">
-                  <div class="card-body main-switch main-switch-color">
-                    <div class="switch-info swich-size">
-                      <input type="checkbox" class="toggle" id="status_tipo" name="status_tipo" value="1">
-                      <label for="status_tipo">Ativo</label>
+                <div class="col-2">
+                  <div class="card-body main-switch main-switch-color switch-unchecked switch_border">
+                    <div class="switch-info switch-unchecked-danger switch-border-info my-3 swich-size">
+                      <input type="checkbox" class="toggle" id="status_uot1" name="status_uot1" <?= $rsUOTipo['status'] == NULL || $rsUOTipo['status'] == 1 ? "checked" : "checked" ;?>>
+                      <label for="status_uot1"> Ativo</label>
                     </div>
                   </div>
                 </div>
               </div>
+              <!-- div row checkbox - END -->
+              <!-- div row buttons - BEGIN -->
               <div class="row">
                 <div class="box-footer text-center">
                   <button type="reset" class="btn btn-danger waves-effect waves-light b-r-22" id="btn_cancelar">
@@ -89,20 +97,24 @@ $rsUnidadeOrganizaionalTipo = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <i class="ti ti-writing"></i> Cadastrar
                   </button>
                 </div>
-                <input type="hidden" id="id" name="id" value="">
+                <input type="hidden" id="id" name="id" value="<?= $rsUOTipo['id'] ;?>">
               </div>
+              <!-- div row buttons - END -->
             </form>
+            <!-- formulário de cadastro - END -->
           </div>
         </div>
       </div>
     </div>
+    <!-- div de cadastro - END -->
+    <!-- div de listagem/dataTable - BEGIN -->
     <div class="row">
       <div class="col-md-12">
         <div class="card ">
           <div class="card-header">
-            <h5><span id="titulo_form" class="icon-name"> LISTA DE TIPOS DE SECRETARIAS</span></h5>
+            <h5><span id="titulo_form" class="icon-name"> LISTA DE TIPOS DE UNIDADES ORGANIZACIONAIS</span></h5>
             <small>Explicação da página</small>
-            <input type="hidden" id="titulo_relatorio" value="Relatório de tipos de secretarias cadastrados no sistema">
+            <input type="hidden" id="titulo_relatorio" value="Relatório de tipos de unidades organizacionais cadastradas no sistema">
           </div>
           <div class="card-body p-0">
             <div class="app-datatable-default overflow-auto">
@@ -119,19 +131,19 @@ $rsUnidadeOrganizaionalTipo = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </thead>
                 <tbody>
                   <?php
-                  foreach ($rsUnidadeOrganizaionalTipo as $kObj => $vObj) {
-                    $excluirDisable = !is_null($vObj['id_tipo']) ? 'negado="true" data-toggle="tooltip" title="Este registro não pode ser exlcuido pois está vinculado a um tipo de secretaria!" onclick="return null;"' : '';
+                  foreach ($rsUOTipos as $kObj => $vObj) {
+                    $excluirDisable = $vObj['qtd_uo_id'] > 0 ? 'negado="true" data-toggle="tooltip" title="Este registro não pode ser exlcuido pois está vinculado a uma unidade organizacional!" onclick="return null;"' : 'onclick="btnExcluir(this)"';
                     ?>
                     <tr>
                       <input type="hidden" id="td_id" value="<?= $vObj['id']; ?>">
                       <td id="td_count"><?= $kObj+1; ?></td>
                       <td id="td_nome"><?= $vObj['nome']; ?></td>
-                      <td id="td_status" value="<?= $vObj['status'];?>"><span class="badge text-light-primary"><?= $vObj['status'] == 1 ? 'Ativo' : 'Inativo'; ?></span></td>
+                      <td id="td_status" value="<?= $vObj['status'];?>"><span class="badge <?= $vObj['status'] == 1 ? 'text-light-primary' : 'text-light-warning'; ?> "><?= $vObj['status'] == 1 ? 'Ativo' : 'Inativo'; ?></span></td>
                       <td class="text-center">
                         <button type="button" id="btn_editar_registro" class="btn_editar_registro btn btn-light-warning icon-btn b-r-4" onclick="btnEditar(this);">
                           <i class="ti ti-edit"></i>
                         </button>
-                        <button type="button" id="btn_excluir_registro" class="btn_excluir_registro btn btn-light-danger icon-btn b-r-4" <?= $excluirDisable; ?> onclick="btnExcluir(this);">
+                        <button type="button" id="btn_excluir_registro" class="btn_excluir_registro btn btn-light-danger icon-btn b-r-4" <?= $excluirDisable; ?>>
                           <i class="ti ti-trash"></i>
                         </button>
                       </td>
@@ -154,11 +166,11 @@ $rsUnidadeOrganizaionalTipo = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </div>
       </div>
     </div>
+    <!-- div de listagem/dataTable - END -->
   </div>
 </main>
 <?php
 include_once ('template/footer.php');
-//include_once ('template/control_sidebar.php');
 include_once ('template/rodape.php');
 ?>
 <script type="text/javascript" src="<?= PORTAL_URL; ?>control/bsc/unidade_organizacional_tipo/dashboard.js"></script>
