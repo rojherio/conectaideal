a19b1f58e6836ebae8038f4b099c0d480873bed1
Someone3lse <gabrielmouramarinho@gmail.com>
2025-09-09 22:43:56 -0500

    equipamento_ensino_aprendiz_tipo

---
+++ control/ue/equipamento_ensino_aprendiz_tipo/listar.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+});
+function btnNovo(){
+  postToURL(PORTAL_URL + 'view/ue/equipamento_ensino_aprendiz_tipo/cadastrar/');
+};
+function btnVisualizar(elem){
+  let id = $(elem).parents('tr').children('input#td_id').val();
+  postToURL(PORTAL_URL + 'view/ue/equipamento_ensino_aprendiz_tipo/visualizar/' + id);
+};
+function btnEditar(elem){
+  let id = $(elem).parents('tr').children('input#td_id').val();
+  postToURL(PORTAL_URL + 'view/ue/equipamento_ensino_aprendiz_tipo/cadastrar', {id: id});
+};
+function btnExcluir(elem){
+  let id = $(elem).parents('tr').children('input#td_id').val();
+  let params = {
+    id:             id,
+    urlToSend:      'model/ue/equipamento_ensino_aprendiz_tipo/excluir',
+    urlToGo:        'view/ue/equipamento_ensino_aprendiz_tipo/listar'
+  };
+  ajaxSendExcluir(params);
+}
