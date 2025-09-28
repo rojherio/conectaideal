d0e610fa00d9236f1020f2eda8652c691815669c
Someone3lse <gabrielmouramarinho@gmail.com>
2025-09-28 17:54:27 -0500

    grandeza

--- view/bsc/grandeza/cadastrar.php
+++ view/bsc/grandeza/cadastrar.php
@@ -6,10 +6,7 @@ $id = !(isset($_POST['id'])) ? 0 : $_POST['id'];
 $db = Conexao::getInstance();
 //Consulta para Edição - BEGIN
 $stmt = $db->prepare("SELECT 
-  g.id,
-  g.status,
-  g.dt_cadastro,
-  g.nome
+  g.id
   FROM bsc_grandeza AS g
   WHERE g.id = ? ;");
 $stmt->bindValue(1, $id);
@@ -18,25 +15,16 @@ $rsGrandeza = $stmt->fetch(PDO::FETCH_ASSOC);
 if (!is_array($rsGrandeza)) {
   $rsGrandeza = array();
   $rsGrandeza['id'] = 0;
-  $rsGrandeza['status'] = 1;
-  $rsGrandeza['dt_cadastro'] = '';
-  $rsGrandeza['nome'] = '';
 }
 //Consulta para Edição - END
 //Parámetros de títutlos - BEGIN
 $tituloPagina             = "Cadastro de Grandeza";
 $descricaoPagina          = "Informações de grandeza";
-$tituloFormulario1        = "Dados de Grandeza";
-$descricaoFormulario1     = "Dados de identificação de grandeza";
-$tituloFormulario2        = "";
-$descricaoFormulario2     = "";
-$tituloFormulario3        = "";
-$descricaoFormulario3     = "";
-$tituloFormulario4        = "";
-$descricaoFormulario4     = "";
-$tituloFormulario5        = "Situação";
-$descricaoFormulario5     = "Defina se esse cadastro de grandeza está ativo ou inativo";
 //Parámetros de títutlos - END
+//Parámetros de Exibição de campos - BEGIN
+$exibeSituação            = true;
+$exibeButões              = true;
+//Parámetros de Exibição de campos - END
 ?>
 <!-- Main Section - BEGIN-->
 <main>
@@ -61,90 +49,10 @@ $descricaoFormulario5     = "Defina se esse cadastro de grandeza está ativo ou 
     </div>
     <!-- div Título página e links de navegação - END -->
     <!-- formulário de cadastro - BEGIN -->
-    <form class="app-form" id="form_grandeza" name="form_grandeza" method="post" action="">
-      <input type="hidden" name="g_id" id="g_id" value="<?= $rsGrandeza['id'] ;?>">
-      <!-- div de cadastro - BEGIN -->
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-header">
-              <!-- Título da div de cadastro - BEGIN -->
-              <h5><?= $tituloFormulario1;?></h5>
-              <small><?= $descricaoFormulario1;?></small>
-              <!-- Título da div de cadastro - END -->
-            </div>
-            <div class="card-body">
-              <!-- div row input - BEGIN -->
-              <div class="row">
-                <?= createInput(array(
-                  /*int 1-12*/  'col'         => 12,
-                  /*string*/    'label'       => 'Nome',
-                  /*string*/    'type'        => 'text',
-                  /*string*/    'name'        => 'g_nome',
-                  /*string*/    'id'          => 'g_nome',
-                  /*string*/    'class'       => 'form-control',
-                  /*int*/       'minlength'   => 3,
-                  /*int*/       'maxlength'   => 45,
-                  /*string*/    'placeholder' => 'Digite o nome da grandeza',
-                  /*string*/    'value'       => $rsGrandeza['nome'],
-                  /*bool*/      'required'    => true,
-                  /*string*/    'prop'        => ''
-                )) ;?>
-              </div>
-              <!-- div row input - END -->
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-header">
-              <!-- Título da div de cadastro - BEGIN -->
-              <h5><?= $tituloFormulario5;?></h5>
-              <small><?= $descricaoFormulario5;?></small>
-              <!-- Título da div de cadastro - END -->
-            </div>
-            <div class="card-body">
-              <!-- div row input - BEGIN -->
-              <div class="row">
-                <?= createCheckbox(array(
-                  /*int 1-12*/  'col'         => 12,
-                  /*string*/    'label'       => 'Ativo',
-                  /*string*/    'type'        => 'checkbox',
-                  /*string*/    'name'        => 'g_status',
-                  /*string*/    'id'          => 'g_status',
-                  /*string*/    'class'       => 'toggle',
-                  /*string*/    'value'       => 1,
-                  /*string*/    'checked'     => $rsGrandeza['status'],
-                  /*string*/    'prop'        => ''
-                )) ;?>
-              </div>
-              <!-- div row input - END -->
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-body">
-              <!-- div row buttons - BEGIN -->
-              <div class="row">
-                <div class="box-footer text-center">
-                  <button type="reset" class="btn btn-outline-danger b-r-22" id="btn_cancelar">
-                    <i class="ti ti-eraser"></i> Cancelar
-                  </button>
-                  <button type="button" id="submit" class="btn btn-outline-success waves-light b-r-22">
-                    <i class="ti ti-writing"></i> Cadastrar
-                  </button>
-                </div>
-              </div>
-              <!-- div row buttons - END -->
-            </div>
-          </div>
-        </div>
-      </div>
+    <form class="app-form" id="form_grandeza" name="form_grandeza" method="post" urltosend="bsc/grandeza/salvar_identificacao" action="">
+      <?php 
+      include_once ('view/bsc/grandeza/content_identificacao.php'); 
+      ?>
     </form>
     <!-- formulário de cadastro - END -->
     <!-- div de cadastro - END -->
